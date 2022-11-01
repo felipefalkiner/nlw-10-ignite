8c726b14cde5d13d4869b3898b0d01da28b438f6
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-11-01 20:57:44 -0300

    fetch example

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,12 @@
-import { Tweet } from "../components/Tweet";
-
 export default function Home() {
+
+  fetch('http://localhost:3333/pools/count')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+
   return (
-    <div>
-      <Tweet text="Meu Primeiro Tweet" />
-      <Tweet text="Meu Segundo Tweet" />
-      <Tweet text="Meu Terceiro Tweet" />
-    </div>
+    <h1>Hello Falk</h1>
   )
 }
