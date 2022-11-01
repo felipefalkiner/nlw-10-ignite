7081a1277d0fc6e66acd4e9d98076c22b09d6794
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-11-01 20:05:52 -0300

    update on server.ts and add watch flag to run script on package.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,11 @@ async function bootstrap() {
         logger: true,
     })
 
+    // http://localhost:3333
+    fastify.get('/pools/count', () => {
+        return {count: 123456789}
+    })
+
     await fastify.listen({port: 3333})
 }
 
