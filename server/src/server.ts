93517a0d42d80e92e804aa8d3026492a9a7fc86d
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-11-01 20:21:50 -0300

    update on server.ts, add pools count

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,9 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
@@ -6,8 +11,10 @@ async function bootstrap() {
     })
 
     // http://localhost:3333
-    fastify.get('/pools/count', () => {
-        return {count: 0}
+    fastify.get('/pools/count', async () => {
+        const pools = await prisma.pool.count();
+
+        return {count: pools}
     })
 
     await fastify.listen({port: 3333})
