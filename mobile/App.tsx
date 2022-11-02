07bf004ed6a731054f9dc0d96cac8826fa5a65b3
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-11-02 00:00:56 -0300

    add fonts, Loading component and SignIn screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Text, Center } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>
-          Hello World!
-        </Text>
-        <StatusBar style="auto" />
-      </Center>
+    <NativeBaseProvider theme={THEME}>
+        {
+          fontsLoaded ? <SignIn /> : <Loading />
+        }
     </NativeBaseProvider>
   );
 }
