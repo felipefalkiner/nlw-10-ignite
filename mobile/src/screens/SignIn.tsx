36d76e48f529e3eeae4d19f0527b21b570e6ea2f
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-11-03 12:35:46 -0300

    created Button component and added to the SignIn screen!

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,20 @@
-import { Text, Center } from 'native-base';
+import { Text, Center, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons'
+
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button';
 
 export function SignIn(){
     return(
         <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24} fontFamily="heading">
-          Sign In!
-        </Text>
+        <Logo width={212} height={40} />
+        
+        <Button
+          title="ENTRAR COM O GOOGLE"
+          leftIcon={<Icon as={Fontisto} name="google" color={"white"} size={"md"}/>}
+        />
+        
+
       </Center>
     )
 }
