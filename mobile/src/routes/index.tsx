b7c80c3a27a24dea724eb9363a454f39ccaa7c1b
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-11-06 13:39:21 -0300

    if user is logged in show him to the routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -4,11 +4,15 @@ import { useAuth } from '../hooks/useAuth'
 
 import { AppRoutes } from './app.routes';
 import { SignIn } from '../screens/SignIn';
+import { Box } from 'native-base';
 
 export function Routes(){
+    const { user } = useAuth();
     return(
-        <NavigationContainer>
-            <SignIn />
-        </NavigationContainer>
+        <Box flex={1} bg="gray.900">
+            <NavigationContainer>
+                {user.name ? <AppRoutes /> : <SignIn />}
+            </NavigationContainer>
+        </Box>
     );
 }
