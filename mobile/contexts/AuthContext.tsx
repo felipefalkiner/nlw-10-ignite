84af2f1e2041dea6419d069247391a57309c556f
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-11-06 18:33:21 -0300

    removed the googleClientId imports from the app

--- mobile/contexts/AuthContext.tsx
+++ mobile/contexts/AuthContext.tsx
@@ -2,7 +2,6 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
-import { googleClientId } from './googleClientId'
 import { api } from '../services/api'
 
 WebBrowser.maybeCompleteAuthSession();
